f042387be7152b26dfa851944d5a786915289895
Rama <ramaadjiprsty@gmail.com>
2024-12-29 21:50:18 +0700

    feat: add .env file to store Supabase URL and API key
    - Added .env file to securely store the Supabase API key and URL.
    - Updated project setup instructions to include environment variable configuration.
    - Ensured .env is added to .gitignore to protect sensitive information.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,12 +2,9 @@ import 'react-native-url-polyfill';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { AppState } from 'react-native';
+import { SUPABASE_KEY, SUPABASE_URL } from '@env';
 
-const supabaseUrl = 'https://afbmhlvigsgxkpoffcvg.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmYm1obHZpZ3NneGtwb2ZmY3ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0NTY3NjUsImV4cCI6MjA1MTAzMjc2NX0.yN6iqQxWGj9YHo3J77qr8XcOUWui7vcpEopukhV4XdA';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
